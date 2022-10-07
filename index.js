9d920da7b8841accf7d88b35b83242565d481225
Cryptonized <56192561+ItzCryptonized@users.noreply.github.com>
2022-10-07 20:54:59 +0100

    Add missing package

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Routes, EmbedBuilder, ButtonBuilder, ButtonStyle, InteractionType, Activ
 const fs = require('fs')
 const figlet = require('figlet');
 const { token, footer } = require('./config.json');
+const undici = require('undici')
 const { stripeSecretKey, port, paypalSandboxOrLive, paypalClientSecret, paypalClientID, currency, invoiceSuccessUrl, globalCustomerRoles, serverID } = require('./config.json').storeBot
 const { fromEmail, sendgridApiKey, sendEmails } = require('./config.json').storeBot.email
 const app = require('express')()
